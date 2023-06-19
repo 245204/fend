f8abe1518786dd6390adca8b2c076523c7baef11
245204 <245204@ust.com>
2023-06-19 18:35:38 +0530

    first commit

---
+++ capstone/src/app/admin/model/loginsignup1.ts
@@ -0,0 +1,10 @@
+export class loginsignup1{
+    id:number|any;
+    username:string|undefined;
+    password:number|undefined;
+    firstname:string|undefined;
+    lastname:string|undefined;
+    // conpassword:string|undefined;
+    //type:string|undefined;
+   
+}
